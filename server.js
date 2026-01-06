229240df06ba6e1a614cca11a01851a301c3f56a
Rylogix <rylandotson@gmail.com>
2026-01-06 01:56:27 -0500

    Update site

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mimeTypes = {
   ".css": "text/css; charset=utf-8",
   ".js": "application/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
   ".svg": "image/svg+xml",
 };
 
